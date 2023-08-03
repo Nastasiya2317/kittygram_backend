4d80c6ccddc150bbe9db1942b64e45c23f67e68b
Анастасия Крюкова <Nastasiya2317@yandex.ru>
2023-08-03 18:17:25 +0300

    Text

--- cats/serializers.py
+++ cats/serializers.py
@@ -1,4 +1,4 @@
-import base64
+import base64 # Модуль с функциями кодирования и декодирования base64
 
 from django.core.files.base import ContentFile
 from rest_framework import serializers
@@ -28,13 +28,20 @@ class AchievementSerializer(serializers.ModelSerializer):
         model = Achievement
         fields = ('id', 'achievement_name')
 
-
+# преобразуем текст в картинку через Base64
 class Base64ImageField(serializers.ImageField):
     def to_internal_value(self, data):
+        # Если полученный объект строка, и эта строка 
+        # начинается с 'data:image'...
         if isinstance(data, str) and data.startswith('data:image'):
+            # ...начинаем декодировать изображение из base64.
+            # Сначала нужно разделить строку на части.
+            # data:[<MIME-type>][;base64],<data>  - формат в общем виде
+            # data:image/png;base64,iVBO
             format, imgstr = data.split(';base64,')
             ext = format.split('/')[-1]
-
+            # Затем декодировать сами данные и поместить результат в файл,
+            # которому дать название по шаблону.
             data = ContentFile(base64.b64decode(imgstr), name='temp.' + ext)
 
         return super().to_internal_value(data)
